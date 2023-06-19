7c0d5bd64b81c3042040ccad7b0d59aacb692185
MesakDuduCoder <duduaoa99@gmail.com>
2023-06-19 20:53:12 +0530

    Add nav links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
 import Header from './components/Header';
+import Profile from './components/Profile';
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Header />
-    </>
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../css/header.css';
 
 const Header = () => (
   <header>
-    <nav />
+    <nav>
+      <NavLink to="/" activeClassName="active">
+        Rockets
+      </NavLink>
+      <NavLink to="/missions" activeClassName="active">
+        Missions
+      </NavLink>
+      <NavLink to="/profile" activeClassName="active">
+        My Profile
+      </NavLink>
+    </nav>
   </header>
 );
 
