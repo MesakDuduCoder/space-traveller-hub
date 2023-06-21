62337379c9b1896ed6c4e76b9e6f027403d775d9
Munana Clinton <munanaclinton@gmail.com>
2023-06-21 13:10:31 +0200

    Display joined mission to the profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Row, Col } from 'react-bootstrap';
+import '../css/profile.css';
 
 function Profile() {
+  const { missions } = useSelector((store) => store.mission);
+  const joinedMission = missions.filter((mission) => mission.reserved === true);
+  console.log(joinedMission);
   return (
-    <div>Profile</div>
+    <>
+      <div className="profile-container">
+        <div className="missions">
+          <h1>My Missions</h1>
+          <Row>
+            {joinedMission.map((mission) => <Col key={mission.id}>{mission.name}</Col>)}
+          </Row>
+        </div>
+      </div>
+    </>
   );
 }
 
