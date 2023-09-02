42a034e398f611e449c411208d860870a8e6862b
MesakDuduCoder <duduaoa99@gmail.com>
2023-09-02 23:44:25 +0530

    Add test

--- src/index.js
+++ src/index.js
@@ -9,11 +9,9 @@ import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>,
-
 );
